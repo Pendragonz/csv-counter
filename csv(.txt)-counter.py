7c4854430538bf50dd1b52bc463a1e0f4d152515
parekhd <danielparekh@protonmail.com>
2020-04-06 03:35:38 +0100

    fixed bug where duplicates weren't being removed

--- csv(.txt)-counter.py
+++ csv(.txt)-counter.py
@@ -10,9 +10,8 @@ numCounts=[]
 for num in numbers:
 	numCounts.append( [num, numbers.count(num)] )
 
+numCounts=list(set(map(tuple, numCounts)))
 numCounts.sort(key=lambda elem: elem[1])
-
-set(numCounts[0])
 
 with open('sorted.txt', mode='w') as sorted_file:
 	writer=csv.writer(sorted_file, delimiter=',')
